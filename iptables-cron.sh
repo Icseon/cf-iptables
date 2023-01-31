9894f5a2f849d1e3819dbf5d4ccb619a41b8f287
Xander Sterrenburg <me@icseon.com>
2023-01-31 23:36:00 +0100

    Remove the if statement

--- iptables-cron.sh
+++ iptables-cron.sh
@@ -9,32 +9,28 @@ CLOUDFLARE_IP6_RULES=""
 CLOUDFLARE_IP4_ADDRESSES=$(curl -s https://www.cloudflare.com/ips-v4)
 CLOUDFLARE_IP6_ADDRESSES=$(curl -s https://www.cloudflare.com/ips-v6)
 
-if [ "$?" = "0" ]; then
+# Loop through every IPv4 address that we have downloaded
+for line in $CLOUDFLARE_IP4_ADDRESSES; do
 
-    # Loop through every IPv4 address that we have downloaded
-    for line in $CLOUDFLARE_IP4_ADDRESSES; do
-
-        # Add the IPv4 address to the CLOUDFLARE_IP4_ADDRESSES variable
-        CLOUDFLARE_IP4_RULES+="-A INPUT -s $line -p tcp -m multiport --dports 80,443 -j ACCEPT
+    # Add the IPv4 address to the CLOUDFLARE_IP4_ADDRESSES variable
+    CLOUDFLARE_IP4_RULES+="-A INPUT -s $line -p tcp -m multiport --dports 80,443 -j ACCEPT
 "
 
-    done
+done
 
-        # Loop through every IPv6 address that we have downloaded
-    for line in $CLOUDFLARE_IP6_ADDRESSES; do
+# Loop through every IPv6 address that we have downloaded
+for line in $CLOUDFLARE_IP6_ADDRESSES; do
 
-        # Add the IPv6 address to the CLOUDFLARE_IP6_ADDRESSES variable
-        CLOUDFLARE_IP6_RULES+="-A INPUT -s $line -p tcp -m multiport --dports 80,443 -j ACCEPT
+    # Add the IPv6 address to the CLOUDFLARE_IP6_ADDRESSES variable
+    CLOUDFLARE_IP6_RULES+="-A INPUT -s $line -p tcp -m multiport --dports 80,443 -j ACCEPT
 "
 
-    done
+done
 
-    # Load IPv4s
-    eval "cat <<< \"$(</etc/ip4tables.template)\"" > /etc/ip4tables
-    iptables-restore /etc/ip4tables
+# Load IPv4s
+eval "cat <<< \"$(</etc/ip4tables.template)\"" > /etc/ip4tables
+iptables-restore /etc/ip4tables
 
-    # Load IPv6s
-    eval "cat <<< \"$(</etc/ip6tables.template)\"" > /etc/ip6tables
-    ip6tables-restore /etc/ip6tables
-
-fi
+# Load IPv6s
+eval "cat <<< \"$(</etc/ip6tables.template)\"" > /etc/ip6tables
+ip6tables-restore /etc/ip6tables
